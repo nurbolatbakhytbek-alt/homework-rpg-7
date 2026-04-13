61dff91306b1959682feb25808f71906a48dd733
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-04-13 14:57:02 +0500

    add BalancedStrategy with neutral damage/defense calculation

--- src/com/narxoz/rpg/strategy/BalancedStrategy.java
+++ src/com/narxoz/rpg/strategy/BalancedStrategy.java
@@ -1,4 +1,18 @@
 package com.narxoz.rpg.strategy;
 
-public class BalancedStrategy {
+public class BalancedStrategy implements CombatStrategy {
+    @Override
+    public int calculateDamage(int basePower) {
+        return basePower;
+    }
+
+    @Override
+    public int calculateDefense(int baseDefense) {
+        return baseDefense;
+    }
+
+    @Override
+    public String getName() {
+        return "Balanced Strategy";
+    }
 }
